67f8d481268af5133b2ec41295f36dd215a50e93
Roman <rbster@yandex.ru>
2023-05-17 14:37:36 +0300

    test

--- test/SmartWallet.ts
+++ test/SmartWallet.ts
@@ -8,18 +8,11 @@ describe("SmartWallet", function () {
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployOneYearLockFixture() {
-    // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    // const ONE_GWEI = 1_000_000_000;
-
-    // const lockedAmount = ONE_GWEI;
-    // const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-    // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount, secondOtherAccount] = await ethers.getSigners();
-
     const Lock = await ethers.getContractFactory("SmartWallet");
-    const smartWallet = await Lock.deploy();
-
+    const smartWallet = Lock.deploy(
+      // [], 0
+      );
     return { smartWallet, owner, otherAccount, secondOtherAccount };
   }
 
